937c060cbfd9b6d18bf34d7c38046630d40aed01
F0rzend <zhogalkostya@gmail.com>
2022-08-03 14:40:24 +0300

    Add contextual logging

--- copier/stream_copier.go
+++ copier/stream_copier.go
@@ -2,31 +2,29 @@ package copier
 
 import (
 	"bytes"
+	"context"
 	"errors"
+	"github.com/rs/zerolog"
 	"io"
 	"mime"
 	"net/http"
 	"os"
 
 	"github.com/gabriel-vasile/mimetype"
-	"github.com/google/uuid"
-	"github.com/rs/zerolog"
 )
 
 type StreamCopier struct {
 	client *http.Client
-	logger zerolog.Logger
 }
 
 type GetOutputFunc func(ext string) (io.WriteCloser, error)
 
-func NewStreamCopier(client *http.Client, logger zerolog.Logger) *StreamCopier {
+func NewStreamCopier(client *http.Client) *StreamCopier {
 	if client == nil {
 		client = http.DefaultClient
 	}
 	return &StreamCopier{
 		client: client,
-		logger: logger,
 	}
 }
 
@@ -34,9 +32,7 @@ var (
 	ErrStreamClosed = errors.New("stream closed")
 )
 
-func (d *StreamCopier) CopyStream(url string, getOutput GetOutputFunc) error {
-	log := d.logger.With().Str("request_id", uuid.New().String()).Logger()
-
+func (d *StreamCopier) CopyStream(ctx context.Context, url string, getOutput GetOutputFunc) error {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return err
@@ -45,7 +41,7 @@ func (d *StreamCopier) CopyStream(url string, getOutput GetOutputFunc) error {
 	resp, err := d.client.Do(req)
 	defer func() {
 		if err := resp.Body.Close(); err != nil {
-			log.Error().Err(err).Msg("error closing response body")
+			zerolog.Ctx(ctx).Error().Err(err).Msg("error closing response body")
 		}
 	}()
 	if err != nil {
@@ -53,20 +49,20 @@ func (d *StreamCopier) CopyStream(url string, getOutput GetOutputFunc) error {
 	}
 
 	if resp.StatusCode != http.StatusNotFound {
-		d.logger.Debug().Int("status_code", resp.StatusCode).Msg("got response")
+		zerolog.Ctx(ctx).Debug().Int("status_code", resp.StatusCode).Msg("got response")
 	}
 
 	if resp.StatusCode != http.StatusOK {
 		return ErrStreamClosed
 	}
 
-	log.Info().Msg("recording started")
+	zerolog.Ctx(ctx).Info().Msg("recording started")
 
-	fileExtension, body, err := DetectExtension(resp)
+	fileExtension, body, err := DetectExtension(ctx, resp)
 	if err != nil {
 		return err
 	}
-	log.Debug().Str("extension", fileExtension).Msg("detected extension")
+	zerolog.Ctx(ctx).Debug().Str("extension", fileExtension).Msg("detected extension")
 
 	output, err := getOutput(fileExtension)
 	if err != nil {
@@ -74,32 +70,34 @@ func (d *StreamCopier) CopyStream(url string, getOutput GetOutputFunc) error {
 	}
 	defer func() {
 		if err := output.Close(); err != nil {
-			log.Error().Err(err).Msg("error closing output")
+			zerolog.Ctx(ctx).Error().Err(err).Msg("error closing output")
 		}
 	}()
 	if file, ok := output.(*os.File); ok {
-		log.Debug().Str("filename", file.Name()).Msg("output in file")
+		zerolog.Ctx(ctx).Debug().Str("filename", file.Name()).Msg("output in file")
 	}
 
 	bytesCopied, err := io.Copy(output, body)
-	log.Debug().Int64("bytes_copied", bytesCopied).Msg("copied bytes")
+	zerolog.Ctx(ctx).Debug().Int64("bytes_copied", bytesCopied).Msg("copied bytes")
 
-	log.Info().Msg("recording finished")
+	zerolog.Ctx(ctx).Info().Msg("recording finished")
 	return err
 }
 
 // DetectExtension returns response extension by first looking into response headers.
 // As a fallback, it looks into response body and returns the extension and a new
 // body containing the original content.
-func DetectExtension(r *http.Response) (string, io.Reader, error) {
+func DetectExtension(ctx context.Context, r *http.Response) (string, io.Reader, error) {
 	contentType := r.Header.Get("Content-Type")
-	if ext, err := extensionFromContentType(contentType); err == nil {
+	ext, err := extensionFromContentType(ctx, contentType)
+	if err == nil {
 		return ext, r.Body, err
 	}
+	zerolog.Ctx(ctx).Warn().Err(err).Msg("extension detection from bytes")
 	return extensionFromBody(r.Body)
 }
 
-func extensionFromContentType(contentType string) (string, error) {
+func extensionFromContentType(ctx context.Context, contentType string) (string, error) {
 	weightedExtensions := []string{".mp3"}
 
 	acceptableExtensions, err := mime.ExtensionsByType(contentType)
@@ -109,6 +107,10 @@ func extensionFromContentType(contentType string) (string, error) {
 	if len(acceptableExtensions) == 0 {
 		return ".unknown", nil
 	}
+	zerolog.Ctx(ctx).
+		Debug().
+		Interface("extensions", acceptableExtensions).
+		Msg("extensions from content type")
 
 	for _, preferred := range weightedExtensions {
 		for _, option := range acceptableExtensions {
